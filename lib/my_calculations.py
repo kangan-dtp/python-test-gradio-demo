b215e8e14cedc0e7bf8fe4c345b34b70cede6cd3
Anh Nguyen <anguyen@Anhs-MacBook.local>
2025-05-08 13:04:58 +1000

    some stuffs got changed

--- lib/my_calculations.py
+++ lib/my_calculations.py
@@ -4,7 +4,7 @@ class Calculations:
         self.b = b
 
     def get_sum(self):
-        return self.a + self.a
+        return self.a + self.b
 
     def get_difference(self):
         return self.b - self.b
@@ -14,3 +14,20 @@ class Calculations:
 
     def get_quotient(self):
         return self.a % self.b
+
+    # a factorial is the product of all positive integers less than or equal to a number
+    def get_factorial(self):
+        count = 1
+        total = 1
+
+        if self.a < 0:
+            return None
+
+        if type(self.a) is not int:
+            return None
+
+        while count <= self.a:
+            total = total * count
+            count = count + 1
+
+        return total
--- tests.py
+++ tests.py
@@ -8,6 +8,39 @@ class TestCalculations(unittest.TestCase):
     def test_sum(self):
         calculation = Calculations(8, 2)
         self.assertEqual(calculation.get_sum(), 10, "The sum is wrong.")
+        calculation.a = 0
+        self.assertEqual(calculation.get_sum(), 2, "The sum is wrong")
+        calculation.a = -2
+        self.assertEqual(calculation.get_sum(), 0, "The sum is wrong")
+
+        calculation = Calculations(8.1, 2.2)
+        self.assertEqual(calculation.get_sum(), 10.3, "The sum is wrong")
+
+    def test_factorial(self):
+        calculation = Calculations(4, 0)
+        self.assertEqual(calculation.get_factorial(), 24, "The factorial is wrong.")
+        calculation.a = 6
+        self.assertEqual(calculation.get_factorial(), 720, "The factorial is wrong.")
+        calculation.a = 3
+        self.assertEqual(calculation.get_factorial(), 6, "The factorial is wrong.")
+        calculation.a = 0
+        self.assertEqual(
+            calculation.get_factorial(),
+            1,
+            "The factorial is wrong.  Factorial of 0 is 1",
+        )
+        calculation.a = -2
+        self.assertEqual(
+            calculation.get_factorial(),
+            None,
+            "The factorial is wrong.  Factorial of negative number is not defined",
+        )
+        calculation = Calculations(8.2, 0)
+        self.assertEqual(
+            calculation.get_factorial(),
+            None,
+            "The factorial is wrong.  Factorial of decimal number is not defined",
+        )
 
 
 if __name__ == "__main__":
